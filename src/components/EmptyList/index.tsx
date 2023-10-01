0810dbf1477d97f125ecf5b841c28ef1c611dbb5
luizfernando <luiz-fsilva37@hotmail.com>
2023-10-01 02:44:46 -0300

    add component empty list

---
+++ src/components/EmptyList/index.tsx
@@ -0,0 +1,21 @@
+import { Image, Text, View } from 'react-native'
+import { styles } from './styles'
+
+const assets = '../../../assets/'
+
+export function EmptyList() {
+  return (
+    <View style={styles.emptyList}>
+      <Image
+        style={styles.clipboard}
+        source={require(`${assets}clipboard.png`)}
+      />
+      <Text style={styles.info1}>
+        Você ainda não tem tarefas cadastradas.
+      </Text>
+      <Text style={styles.info2}>
+        Crie tarefas e organize seus itens a fazer!
+      </Text>
+    </View>
+  )
+}
---
+++ src/components/EmptyList/styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  emptyList: {
+    borderTopWidth: 1.3,
+    padding: 50,
+    borderTopColor: '#333333',
+    alignItems: 'center',
+  },
+  clipboard: {
+    width: 56,
+    height: 56,
+    resizeMode: 'stretch',
+  },
+  info1: {
+    marginTop: 22,
+    fontSize: 14,
+    color: '#808080',
+    fontWeight: 'bold',
+  },
+  info2: {
+    fontSize: 14,
+    color: '#808080',
+  },
+})
